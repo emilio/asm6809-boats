a3ac14d1d128eb2b1302604a1d451df3792240c8
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-05-14 16:33:18 +0200

    Create _relocatable_presentation.asm

---
+++ _relocatable_presentation.asm
@@ -0,0 +1,29 @@
+;   +--------------------------------------------+
+;   |                PRESENTATION                |
+;   +--------------------------------------------+
+;   | @author Emilio Cobos <emiliocobos@usal.es> |
+;   +--------------------------------------------+
+
+
+			.module flota
+			.globl presentation
+			.globl print
+
+;---------------+
+;   READ-ONLY   |
+;---------------+
+; NO PUEDO ESCRIBIR puntoASCIZ EN COMENTARIOS? EN SERIO?
+; Cuando la cadena sobrepasa cierta longitud (64 bits) `asciz` da una violación de segmento<
+presentation_str:
+			.ascii "\tHundir la flota\t\n------------------------------\n  Emilio Cobos (70912324-N)\n"
+			.byte 0
+
+
+;------------------------------+
+;   SIMPLE ENOUGH, ISN'T IT?   |
+;------------------------------+
+presentation:
+;			ldx	#presentation_str
+			leax	presentation_str,PCR
+			jsr	print
+			rts
